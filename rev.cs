a68ad7259cf72ea8beac1ec3665fdc6d8d8b7b20
Rashan Hasaranga <rashanhasa@gmail.com>
2022-10-31 22:18:24 +0530

    Update rev.cs

--- rev.cs
+++ rev.cs
@@ -6,3 +6,15 @@ System.Net.HttpWebResponse response = request.GetResponse;
 	bool useport = false;
 	string portval = null;
 	Regex reg = default(Regex);
+
+
+if (Form1.ListBox1.SelectedItem.ToString.Contains("http://www.proxynova.com/proxy-server-list/")) {
+		reg = new Regex("<span class=\"row_proxy_ip\">.*");
+		//
+		SplitVar = 1;
+		useport = true;
+		if (Form1.ListBox1.SelectedItem.ToString.Contains("/port-80/")) {
+			portval = "80";
+		} else if (Form1.ListBox1.SelectedItem.ToString.Contains("/port-8080/")) {
+			portval = "8080";
+		}
